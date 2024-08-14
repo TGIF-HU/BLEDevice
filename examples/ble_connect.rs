37f22e488efd4fce29c44d60a8f306a0d80262ce
jijinbei <hallucigenia2002@gmail.com>
2024-08-15 02:25:53 +0900

    永続的に

--- examples/ble_connect.rs
+++ examples/ble_connect.rs
@@ -2,24 +2,24 @@ use esp32_nimble::BLEDevice;
 use esp_idf_svc::hal::task::block_on;
 use log::*;
 
-fn main() -> anyhow::Result<()> {
+fn main() -> ! {
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
     log::set_max_level(log::LevelFilter::Debug);
 
-    block_on(async {
-        let ble_device = BLEDevice::take();
-        let ble_scan = ble_device.get_scan();
-        ble_scan
-            .active_scan(true)
-            .interval(100)
-            .window(99)
-            .on_result(|_scan, param| {
-                info!("Advertised Device: {:?}", param);
-            });
-        ble_scan.start(5000).await?;
-        info!("Scan end");
-
-        Ok(())
-    })
+    loop {
+        block_on(async {
+            let ble_device = BLEDevice::take();
+            let ble_scan = ble_device.get_scan();
+            ble_scan
+                .active_scan(true)
+                .interval(100)
+                .window(99)
+                .on_result(|_scan, param| {
+                    info!("Advertised Device: {:?}", param);
+                });
+            ble_scan.start(5000).await.unwrap();
+            info!("Scan end");
+        });
+    }
 }
