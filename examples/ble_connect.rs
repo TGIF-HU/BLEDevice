16c23451d293909c3cf57fd4b0f2b7d549c60c5f
jijinbei <hallucigenia2002@gmail.com>
2024-08-15 02:08:11 +0900

    微調整

--- examples/ble_connect.rs
+++ examples/ble_connect.rs
@@ -4,7 +4,6 @@ use log::*;
 
 fn main() -> anyhow::Result<()> {
     esp_idf_svc::sys::link_patches();
-
     esp_idf_svc::log::EspLogger::initialize_default();
     log::set_max_level(log::LevelFilter::Debug);
 
